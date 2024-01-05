22a858e1f6b7da1b84ca3fd59f7771686905fe59
Ali Azizi <aliazizi.4973@gmail.com>
2024-01-05 17:22:27 +0330

    Selected Element HTML

--- source/javaScript/app.js
+++ source/javaScript/app.js
@@ -1,2 +1,18 @@
 //* start code javaScript file main (app.js)*
-
+// -> select let
+let $ = document;
+// -> select const
+const body = $.querySelector("body");
+const form = $.querySelector(".form");
+const innerValue = $.querySelector(".input-value");
+const innerDate = $.querySelector(".input-date");
+const countTodo = $.querySelector(".count-todos");
+const filterTodo = $.querySelector(".filter-todo");
+const btnSelectAll = $.querySelector(".btn-all");
+const btnSelectComplete = $.querySelector(".btn-complete");
+const btnSelectUncomplete = $.querySelector(".btn-uncomplete");
+const btnSelectLiked = $.querySelector(".btn-liked");
+const themeDark = $.querySelector(".theme-Dark");
+const clearTodos = $.querySelector(".clear-todo");
+const messageTodoList = $.querySelector(".text-message");
+const dataBaseTodoList = $.querySelector(".dataBase");
