730a9cbc6284980bf1ca9944736791d65cb5c8cb
Ali Azizi <aliazizi.4973@gmail.com>
2024-01-05 18:18:40 +0330

    codeing for part change Theme and save data theme to local storage browser user

--- source/javaScript/app.js
+++ source/javaScript/app.js
@@ -1,6 +1,7 @@
 //* start code javaScript file main (app.js)*
 // -> select let
 let $ = document;
+let themeDark = false;
 // -> select const
 const body = $.querySelector("body");
 const form = $.querySelector(".form");
@@ -12,7 +13,52 @@ const btnSelectAll = $.querySelector(".btn-all");
 const btnSelectComplete = $.querySelector(".btn-complete");
 const btnSelectUncomplete = $.querySelector(".btn-uncomplete");
 const btnSelectLiked = $.querySelector(".btn-liked");
-const themeDark = $.querySelector(".theme-Dark");
+const BtnThemeDark = $.querySelector(".theme-Dark");
 const clearTodos = $.querySelector(".clear-todo");
 const messageTodoList = $.querySelector(".text-message");
 const dataBaseTodoList = $.querySelector(".dataBase");
+const viewportUser = $.querySelector(".viewport-user");
+const viewportDataBase = $.querySelector(".viewport-dataBase");
+const tableData = $.querySelector(".table-data");
+
+
+
+// -> Change Theme Todo List:
+function changeThemeHandler () {
+    // theme Dark true : Theme -> Dark => Light
+    if (themeDark) {
+        body.classList.remove("themeDark");
+        viewportUser.classList.remove("themeDark");
+        viewportDataBase.classList.remove("themeDark");
+        form.classList.replace("form-control-plaintext" , "form-control");
+        tableData.classList.remove("table-dark");
+        themeDark = false;
+    } 
+    // theme Dark false : Theme -> Light => Dark
+    else {
+        // is Dark
+        body.classList.add("themeDark");
+        viewportUser.classList.add("themeDark");
+        viewportDataBase.classList.add("themeDark");
+        form.classList.replace("form-control" , "form-control-plaintext");
+        tableData.classList.add("table-dark");
+        themeDark = true;
+    }
+    // save data theme Dark to local storage:
+    localStorage.setItem("darkMode",JSON.stringify(themeDark));
+}
+
+
+function loadThemeAppHandler () {
+    let theme = JSON.parse(localStorage.getItem("darkMode"));
+    if (theme) {
+        changeThemeHandler();
+    }
+}
+
+
+
+
+// -> add Event Click and...:
+BtnThemeDark.addEventListener("click",changeThemeHandler); 
+window.addEventListener("load",loadThemeAppHandler);
